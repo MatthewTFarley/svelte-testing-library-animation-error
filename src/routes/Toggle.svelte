9a7470f3559d6ef5f60708c16c32f98578357aa6
Matthew Farley <matthewthomasfarley@gmail.com>
2024-04-28 08:36:22 -0400

    Initial commit

---
+++ src/routes/Toggle.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { slide } from "svelte/transition";
+
+  let shouldShow = $state(false);
+
+  function toggleShouldShow() {
+    shouldShow = !shouldShow;
+  }
+</script>
+
+<button onclick={toggleShouldShow}>Click</button>
+
+{#if shouldShow}
+  <div transition:slide={{ duration: 200 }}>Hi</div>
+{/if}
---
+++ src/routes/Toggle.test.ts
@@ -0,0 +1,36 @@
+import { expect, it, vi } from "vitest";
+import { fireEvent, render } from "@testing-library/svelte/svelte5";
+import Toggle from "./Toggle.svelte";
+
+// Mock animate to bypass animation failure
+// Element.prototype.animate ??= vi.fn().mockReturnValue({
+// 	finished: Promise.resolve(),
+// 	cancel: vi.fn(),
+// 	startTime: null,
+// 	currentTime: null
+// });
+
+it('does not show "Hi" on initial render', async () => {
+  const { queryByText } = render(Toggle);
+
+  expect(queryByText("Hi")).toBeFalsy();
+});
+
+it('shows "Hi" when the button is clicked', async () => {
+  const { getByText } = render(Toggle);
+
+  const button = getByText("Click");
+  await fireEvent.click(button);
+  expect(getByText("Hi")).toBeTruthy();
+});
+
+it('does not show "Hi" when the button is clicked twice', async () => {
+  const { getByText, queryByText } = render(Toggle);
+
+  const button = getByText("Click");
+  await fireEvent.click(button);
+  expect(getByText("Hi")).toBeTruthy();
+
+  await fireEvent.click(button);
+  expect(queryByText("Hi")).toBeFalsy();
+});
